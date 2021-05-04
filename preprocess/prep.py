22d02f6d33d7b7692668b17ade93f2072a2f7ef8
Vladislav <vyumoiseenkov@edu.hse.ru>
2021-05-04 16:28:42 +0300

    start preprocess

--- preprocess/prep.py
+++ preprocess/prep.py
@@ -1,7 +1,16 @@
 import pandas as pd
+import numpy as np
+
+
 
 class Preprocess:
+
     def load_data(self):
-        self.df = pd.read_csv('../data/df2.csv')
+        self.df = pd.read_csv('../data/maindata.csv')
+    def info(self):
+        self.n_samples = 2*int(self.df['length'].sum()/ 0.1)
+        self.class_dist = self.df.groupby(['emotion'])['length'].mean()
+        self.prob_dist = self.class_dist / self.class_dist.sum()
+        self.choices = np.random.choice(self.class_dist.index, p = self.prob_dist)
 
 
