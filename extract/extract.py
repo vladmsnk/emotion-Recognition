a50817835ac7a815d3287204b3f03753ef353089
Vladislav <vyumoiseenkov@edu.hse.ru>
2021-05-04 15:27:28 +0300

    extract data

---
+++ extract/extract.py
@@ -0,0 +1,40 @@
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
+import numpy as np
+from scipy.io import wavfile
+
+class Extract:
+    def __init__(self, path):
+        self.path = path
+
+    def extract(self):
+        filenames = [f for f in listdir(self.path) if isfile(join(self.path, f))]
+        emotion = pd.DataFrame(filenames).loc[:, 0].apply(lambda x: x.split('_')[2])
+        filnm = pd.Series(filenames)
+        maindata = pd.concat({"filename": filnm, "emotion": emotion}, axis=1)
+        maindata.set_index('filename', inplace=True)
+        return maindata
+
+    def makedata(self):
+        self.data = self.extract()
+
+    def add_len(self):
+        for f in self.data.index:
+            rate, signal = wavfile.read(self.path+f)
+            self.data.at[f,'length'] = signal.shape[0] / rate #signal per second
+
+    def add_info(self):
+        self.classes = list(np.unique(self.data.emotion))
+        self.class_distr = self.data.groupby(['emotion'])['length'].mean()
+
+
+    def save_data(self):
+        self.data.to_csv('../data/maindata.csv')
+
+    def __call__(self):
+        self.extract()
+        self.makedata()
+        self.add_len()
+        self.data_info()
+        self.save_data()
