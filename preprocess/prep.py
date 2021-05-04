b6adb4b0ea73578329227a53a8c5b0b10ec34afc
Vladislav <vyumoiseenkov@edu.hse.ru>
2021-05-04 17:24:24 +0300

    prep almost complited

--- preprocess/prep.py
+++ preprocess/prep.py
@@ -3,7 +3,8 @@ import numpy as np
 from tqdm import tqdm
 from scipy.io import wavfile
 from configs.config import Config
-
+from python_speech_features import mfcc, logfbank
+from keras.utils import np_utils
 
 
 class Preprocess(Config):
@@ -14,6 +15,8 @@ class Preprocess(Config):
         self.class_dist = self.df.groupby(['emotion'])['length'].mean()
         self.prob_dist = self.class_dist / self.class_dist.sum()
         self.choices = np.random.choice(self.class_dist.index, p = self.prob_dist)
+        self.classes = list(np.unique(self.df.emotion))
+
     def build_rand_feat(self):
         X = []
         y = []
@@ -24,4 +27,16 @@ class Preprocess(Config):
             rate, wav = wavfile.read(self.audpath + file)
             emotion = self.df.at[file, 'emotion']
             rand_index = np.random.randint(0, wav.shape[0] - self.step)
+            sample = wav[rand_index: rand_index + self.step]
+            X_sample = mfcc(sample, rate, numcep= self.nfeat, nfilt = self.nfilt , nfft = self.nfft).T
+            minimum = min(np.amin(X_sample, minimum))
+            maximum = max(np.amax(X_sample, maximum))
+            X.append(X_sample)
+            y.append(self.classes.index(emotion))
+        X, y = np.array(X), np.array(y)
+        X = (X - minimum) / (maximum - minimum)
+        X = X.reshape(X.shape[0], X.shape[1], X.shape[2],1)
+        y = np_utils.to_categorical(y,num_classes =10 )
+        return X, y
+
 
