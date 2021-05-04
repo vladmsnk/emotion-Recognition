6e7402ac072aa6da723e0a3dd9937ee277a0bcf8
Vladislav <vyumoiseenkov@edu.hse.ru>
2021-05-04 17:25:35 +0300

    edit config again

--- configs/config.py
+++ configs/config.py
@@ -1,12 +1,12 @@
 
 
 class Config:
-    def __init__(self, nfilt = 26, nfeat = 13, nfft = 512 , rate = 16000):
+    def __init__(self, nfilt = 26, nfeat = 13, nfft = 512 , ratec = 16000):
         self.nfilt = nfilt
         self.nfeat = nfeat
         self.nfft = nfft
-        self.rate = rate
-        self.step = int(rate/10)
+        self.ratec = ratec
+        self.step = int(ratec /10)
         self.audpath = '../data/AudioWAV/'
         self.savedatapath = '../data/maindata.csv'
 
