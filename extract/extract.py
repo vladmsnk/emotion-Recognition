5087a477d57b58de7076282e4482bb9c0c8b1f04
Vladislav <moiseenkov-v@mail.ru>
2021-05-22 21:36:52 +0300

    change a bit(second)

--- extract/extract.py
+++ extract/extract.py
@@ -1,18 +1,24 @@
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, exists
 import pandas as pd
 import numpy as np
 from scipy.io import wavfile
 from configs.config import Config
 
+
 class Extract():
-    def extract(self):
+    def __extract(self):
         filenames = [f for f in listdir(Config.audpath) if isfile(join(Config.audpath, f))]
         emotion = pd.DataFrame(filenames).loc[:, 0].apply(lambda x: x.split('_')[2])
         filnm = pd.Series(filenames)
         maindata = pd.concat({"filename": filnm, "emotion": emotion}, axis=1)
         maindata.set_index('filename', inplace=True)
         self.data = maindata
+    def get_data(self):
+        if exists(Config.savedatapath):
+            return pd.read_csv(Config.savedatapath)
+        else:
+            print("maindata.csv was not created!")
 
     def __add_len(self):
         for f in self.data.index:
@@ -32,8 +38,10 @@ class Extract():
         self.data.loc[(self.data['emotion'] == 'FEA'), 'emotion'] = 'SAD'
 
     def __call__(self):
-        self.extract()
+        self.__extract()
         self.__add_len()
         self.__add_info()
         self.__change1()
         self.__save_data()
+e = Extract()
+e()
