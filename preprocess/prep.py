c4debccaeaa053212534e2c22a3721e42ba1aa83
Vladislav <moiseenkov-v@mail.ru>
2021-05-23 00:20:52 +0300

    final prep

--- preprocess/prep.py
+++ preprocess/prep.py
@@ -9,58 +9,68 @@ import os
 import pickle
 
 
-class Preprocess(Config):
-    def load_data(self):
-        self.df = pd.read_csv(self.savedatapath)
+class Preprocess():
+
+    def __load_data(self):
+        self.df = pd.read_csv(Config.savedatapath)
         self.df.set_index('filename',inplace =True)
-    def _info(self):
+
+    def __info(self):
         self.n_samples = 2*int(self.df['length'].sum()/ 0.1)
         self.class_dist = self.df.groupby(['emotion'])['length'].mean()
         self.prob_dist = self.class_dist / self.class_dist.sum()
         self.choices = np.random.choice(self.class_dist.index, p = self.prob_dist)
         self.classes = list(np.unique(self.df.emotion))
 
-    def __check_data(self):
-        if os.path.isfile(self.p_path):
-            print("Loading existing date for model")
-            with open(self.p_path, 'rb') as handle:
-                tmp =pickle.load(handle)
-                return tmp
-        else:
-            return None
+    def get_info(self):
+        return (self.n_samples,self.class_dist,self.prob_dist,self.choices,self.classes)
+    # def __check_data(self):
+    #     if os.path.isfile(Config.p_path):
+    #         print("Loading existing date for model")
+    #         with open(Config.p_path, 'rb') as handle:
+    #             tmp =pickle.load(handle)
+    #             return tmp
+    #     else:
+    #         return None
 
     def build_rand_feat(self):
-        tmp = self.__check_data()
-        if tmp:
-            return tmp.data[0],tmp.data[1]
+        # tmp = self.__check_data()
+        # if tmp:
+        #     return tmp.data[0],tmp.data[1]
         X = []
         y = []
         minimum, maximum = float('inf'), -float('inf')
         for _ in tqdm(range(self.n_samples)):
             rand_class = np.random.choice(self.class_dist.index, p = self.prob_dist)
             file = np.random.choice(self.df[self.df.emotion == rand_class].index)
-            rate, wav = wavfile.read(self.audpath + file)
+            rate, wav = wavfile.read(Config.audpath + file)
             emotion = self.df.at[file, 'emotion']
-            rand_index = np.random.randint(0, wav.shape[0] - self.step)
-            sample = wav[rand_index: rand_index + self.step]
-            X_sample = mfcc(sample, rate, numcep= self.nfeat, nfilt = self.nfilt , nfft = self.nfft)
+            rand_index = np.random.randint(0, wav.shape[0] - Config.step)
+            sample = wav[rand_index: rand_index + Config.step]
+
+            X_sample = mfcc(sample, rate, numcep= Config.nfeat, nfilt = 26, nfft = Config.nfft).T
             minimum = min(np.amin(X_sample), minimum)
             maximum = max(np.amax(X_sample), maximum)
-            Config.min = minimum
-            Config.max = maximum
+            # Config.min = minimum
+            # Config.max = maximum
             X.append(X_sample)
             y.append(self.classes.index(emotion))
         X, y = np.array(X), np.array(y)
         X = (X - minimum) / (maximum - minimum)
         X = X.reshape(X.shape[0], X.shape[1], X.shape[2],1)
         y = np_utils.to_categorical(y,num_classes =4)
-        Config.data = (X,y)
-        with open(self.p_path, 'wb') as handle:
-            pickle.dump(Config, handle, protocol=2)
+        with open(Config.p_path1, 'wb') as handle1:
+            pickle.dump(X, handle1, protocol=2)
+        with open(Config.p_path2, 'wb') as handle2:
+            pickle.dump(y, handle2, protocol=2)
         return X, y
 
     def __call__(self):
-        self.load_data()
-        self._info()
+        self.__load_data()
+        self.__info()
+
+p = Preprocess()
+p()
+X,y = p.build_rand_feat()
 
 
