8bfa0bf03a03a84593b84f0c7410517f0c66a0c0
Vladislav <moiseenkov-v@mail.ru>
2021-05-23 00:22:35 +0300

    final major

--- test/major.py
+++ test/major.py
@@ -1,27 +1,34 @@
 from extract.extract import Extract
-from preprocess.prep import Preprocess
 from build.buildm import Build
 from configs.config import Config
 from sklearn.model_selection import train_test_split
 from keras.callbacks import ModelCheckpoint
+import pickle
+from keras.models import load_model
 
 
 
-conf = Config()
-extr = Extract(conf.audpath)
-prep = Preprocess()
-extr.__call__()
-prep.__call__()
-X,y = prep.build_rand_feat()
+
+with open('../pickle/pickle1.p', 'rb') as handle:
+    X = pickle.load(handle)
+
+with open('../pickle/pickle2.p', 'rb') as handle:
+    y = pickle.load(handle)
+
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33,\
-                                          random_state=42)
-buil = Build(X_train,y_train)
-buil.__call__()
-model = buil.creat_conv_model()
-checkpoint = ModelCheckpoint(conf.model_path,monitor='val_acc',verbose=1,mode= 'max',\
-                             save_best_only = True, save_weights_only = False, period =1 )
-model.fit(X_train, y_train, epochs=conf.n_epoch, batch_size=conf.batch_size,\
-          shuffle= True,validation_data=0.1, callbacks=[checkpoint])
-model.save(conf.model_path)
+                                                    random_state=42)
+
+# buil = Build(X_train,y_train)
+# model = buil.creat_conv_model()
+# checkpoint = ModelCheckpoint(Config.model_path,monitor='val_acc',verbose=1,mode= 'max',\
+#                             save_best_only = True, save_weights_only = False, period =1 )
+# model.fit(X_train, y_train, epochs=Config.n_epoch, batch_size=Config.batch_size,shuffle= True, callbacks=[checkpoint])
+# model.save(Config.model_path)
+
+
+model = load_model('../model/mod.model')
+loss, acc = model.evaluate(X_test, y_test, verbose=2)
+print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
 
 
